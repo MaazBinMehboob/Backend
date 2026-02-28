6d47f66e0bbdfb71fc7cc4ac42ea711304007d84
MaazBinMehboob <maazbinmehboob@gmail.com>
2026-02-28 22:18:04 +0500

    updated

--- src/app.js
+++ src/app.js
@@ -16,7 +16,7 @@ const PORT = process.env.PORT || 5000
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-  origin: process.env.FRONTEND_URL || "http://localhost:5173",
+  origin: "https://maazbinmehboob-hackathon.vercel.app",
   credentials: true
 }));
 // app.use(helmet());
